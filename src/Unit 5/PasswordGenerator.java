34eeab594791664663bea4993a5472018c4babe4
Nguyen Pham <unknownuser2041@gmail.com>
2024-03-15 22:23:36 -0400

    commit

--- src/Unit 5/PasswordGenerator.java
+++ src/Unit 5/PasswordGenerator.java
@@ -10,26 +10,26 @@ public class PasswordGenerator {
 		s3 = userInput.nextLine();
 		
 		str = s1.substring(0, 1).toUpperCase();
-	        str += s2.substring(0, 1).toUpperCase();
-	        str += s3.substring(0, 1).toUpperCase();
-	        
-	        str += Math.abs(s1.length() - s2.length());
-	        
-	        int i = s1.indexOf("a");
-	    	if (i%2 == 0 && i < 10) str += i;
-	    	else str += '#';
-	        
-	        s3 = s3.replaceAll("\\s+", "").toLowerCase();
-	        int s3len = s3.length();
-	        if (s3len >= 3) 
-			str += s3.substring(s3len - 3, s3len);
-	        else {
-	            str += s3;
-	            for (int j = 0; j < 3 - s3len; j++) {
-	                str += '%';
-	            }
-	        }
+		str += s2.substring(0, 1).toUpperCase();
+		str += s3.substring(0, 1).toUpperCase();
 		
-	        System.out.println(str);
+		str += Math.abs(s1.length() - s2.length());
+		
+		int i = s1.indexOf("a");
+		if (i%2 == 0 && i < 10) str += i;
+		else str += '#';
+		
+		s3 = s3.replaceAll("\\s+", "").toLowerCase();
+		int s3len = s3.length();
+		if (s3len >= 3) 
+			str += s3.substring(s3len - 3, s3len);
+		else {
+			str += s3;
+			for (int j = 0; j < 3 - s3len; j++) {
+				str += '%';
+			}
+		}
+	
+		System.out.println(str);
 	}
 }
--- src/Unit 5/Scores.java
+++ src/Unit 5/Scores.java
@@ -4,24 +4,24 @@ public class Scores {
 	public static Scanner userInput = new Scanner(System.in);
 	public static void main(String[] args) {
 		
-		int[] occurances = new int[11];
+		int[] occ = new int[11];
 		int score = 1;
 		double sum = 0.0, count = 0.0;
 		
 	    while (score > 0) {
 	    	score = userInput.nextInt();
 	    	if (score > 0 && score <= 10) {
-	    		occurances[score] ++;
+	    		occ[score] ++;
 	    		sum += score;
 	    		count ++;
 	    	}
 	    }
-	    
 	    System.out.println("Score   #Occurrences");
-        for(int i=1; i<10; i++){
-            System.out.println(i + "       " + occurances[i]);
-        } System.out.println(10 + "      " + occurances[10]);
-        
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i + "       " + occ[i]);
+        } 
+		System.out.println(10 + "      " + occ[10]);
         double avg = Math.round(sum/count*10.0)/10.0;
         System.out.println("Mean: " + avg);
     }
